0ccb6c01ce1739d83dbfa06e18903c271417553d
Abhinav Bhandari <abhinav.bhandari@mail.mcgill.ca>
2020-01-05 20:22:11 +0900

    build ng prod and edit package json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAWHllOz2_NWtZ_smlHTq7PMazT3fOW3IY",
+    authDomain: "abhinavblog-1391d.firebaseapp.com",
+    databaseURL: "https://abhinavblog-1391d.firebaseio.com",
+    projectId: "abhinavblog-1391d",
+    storageBucket: "abhinavblog-1391d.appspot.com",
+    messagingSenderId: "221562930916",
+    appId: "1:221562930916:web:920c0924036489cbeccef1",
+    measurementId: "G-GS77X9TF8C"
+  }, 
 };
